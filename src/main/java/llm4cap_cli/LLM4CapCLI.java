f40fbd713e747b5360a804bbab88ff2c3c732c0d
Miguel Vieira <miguel_vieira@gmx.de>
2024-05-15 12:55:09 +0200

    CLI for transferring the natural language description of a capability to the LLM4Cap method.

---
+++ src/main/java/llm4cap_cli/LLM4CapCLI.java
@@ -0,0 +1,59 @@
+package llm4cap_cli;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+		name = "LLM4Cap",
+		mixinStandardHelpOptions = true, 
+		description = "Method to automatically generate and verify capability ontology generated from a natural language description of the capability with an LLM",
+		version = "0.0.1 SNAPSHOT"
+		)
+public class LLM4CapCLI implements Callable<Integer>{
+	
+	@Option(names = {"-f", "--file"}, paramLabel = "FILE", description = "Natural language description of a capability to be generated written in a file.")
+    private File file; 
+	
+	@Parameters(description = "Natural language description of a capability to be generated.", arity = "0..1")
+	private String input; 
+	
+	@Option(names = { "-o", "--out" }, description = "Output file (default: print to console)")
+	private File outputFile; 
+	
+	@Option(names = { "-l", "--llm" }, description = "LLM to use to generate capability ontology (default: Claude)")
+	private String llm = "Claude"; 
+	
+	@Option(names = { "-h", "--help", "-?", "-help"}, usageHelp = true, description = "Display a help message")
+    private boolean help = false;
+
+
+	@Override
+	public Integer call() throws Exception {
+		if (file != null) {
+			try {
+				String nLDescription = new String(Files.readAllBytes(file.toPath()));
+				System.out.println("NL Description: " + nLDescription);
+			} catch (IOException e) {
+				System.err.println("Error reading file: " + e.getMessage());
+				return 1; 
+			}
+		} else if (input != null) {
+			System.out.println("NL Description: " + input);
+		} else {
+			System.out.println("No NL description of a capability provided. ");
+		}
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		int exitCode = new CommandLine(new LLM4CapCLI()).execute(args); 
+		System.exit(exitCode);
+	}
+}
