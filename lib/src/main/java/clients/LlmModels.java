62461b3f2d26c1d034592fd719ec140b16d80ddb
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-06-24 14:32:41 +0200

    Adds more models

---
+++ lib/src/main/java/clients/LlmModels.java
@@ -0,0 +1,12 @@
+package clients;
+
+public enum LlmModels {
+		claude_3_5_sonnet_20240620,
+		claude_3_opus_20240229,
+		claude_3_sonnet_20240229,
+		claude_3_haiku_20240307,
+		gpt_4o,
+		gpt_4_turbo,
+		gpt_4,
+		gpt_3_5_turbo
+}
