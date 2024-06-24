519e5d93489803f93b65f218673bab30b1d304df
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-06-24 14:36:12 +0200

    minor refactoring

--- lib/src/main/java/prompting/PromptGeneration.java
+++ lib/src/main/java/prompting/PromptGeneration.java
@@ -9,13 +9,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+
 public class PromptGeneration {
 
-	static String generatePrompt(String nLDescription) throws IOException {
+	/**
+	 * Takes the prompt template and fills it with all the content for T-Box, examples and the actual task description
+	 * @param taskDescription The natural language task description used to generate a capability description
+	 * @return
+	 * @throws IOException thrown in case the prompt-template file cannot be found or opened
+	 */
+	public static String generatePrompt(String taskDescription) {
+		// Open template file
+		String prompt = null;
+		prompt = readResourceFile("prompt-template.txt");
 		
-		String prompt = readResourceFile("prompt-template.txt"); 
+		// Put all placeholders and their contents in a map
 		Map<String, String> placeholders = new HashMap<String, String>();
-
         placeholders.put("${T-Box}", readResourceFile("cask_cropped.ttl"));
         placeholders.put("${Example Task 1}", readResourceFile("coffeemaking-task.txt"));
         placeholders.put("${Example Capability A-Box 1}", readResourceFile("coffeemaking.ttl"));
@@ -23,19 +32,26 @@ public class PromptGeneration {
         placeholders.put("${Example Capability A-Box 2}", readResourceFile("multiplication.ttl"));
         placeholders.put("${Example Task 3}", readResourceFile("distillation-task.txt"));
         placeholders.put("${Example Capability A-Box 3}", readResourceFile("distillation.ttl"));
-        placeholders.put("${Task}", nLDescription);
+        placeholders.put("${Task}", taskDescription);
 
+        // Replace each placeholder with its value 
         for (Map.Entry<String, String> entry : placeholders.entrySet()) {
             prompt = prompt.replace(entry.getKey(), entry.getValue());
         }
         
+        // Return completed prompt without placeholders
         return prompt; 
 	}
 
-	private static String readResourceFile(String fileName) throws IOException {
-		try (InputStream input = PromptingHelper.class.getClassLoader().getResourceAsStream("prompt/" + fileName);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
-	            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-		}
+	/**
+	 * Little utility function to open files and read them as a String
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 */
+	private static String readResourceFile(String fileName) {
+		InputStream input = PromptGeneration.class.getClassLoader().getResourceAsStream("prompt/" + fileName);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
 	}
 }
